33d3ea2ec90727d0ab8375bba25e9b517be2cf11
sattwik742 <sattwik211@gmail.com>
2025-06-17 16:49:42 +0530

    Second Commit

--- src/Main.c
+++ src/Main.c
@@ -1,2 +1,17 @@
 #include<stdio.h>
 
+int main()
+{
+    int choice;
+
+    printf("\n***************** WELCOME TO BUS RESERVATION SYSTEM ***************\n");
+    printf("Read the below instructions carefully.\n");
+    printf("1.View Bus details.\n");
+    printf("2.Book seat.\n");
+    printf("3.Exit.\n");
+
+    printf("Enter your choice: \n");
+    scanf("%d",choice);
+
+    
+}
